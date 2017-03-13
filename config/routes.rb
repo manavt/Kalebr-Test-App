41d43205d5887f171a9d4ea9495e99d199998993
manavt <manav.manav.smce@gmail.com>
2017-03-13 15:09:19 +0530

    added category page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+
   resources :products
+  resources :categories
   devise_for :users
   root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

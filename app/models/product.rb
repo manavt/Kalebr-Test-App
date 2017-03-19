d4c0a647e12fc9c86d15c0ac677f400e31afbdf9
manavt <manav.manav.smce@gmail.com>
2017-03-19 20:33:28 +0530

    updated Readme.md

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,25 +5,14 @@ class Product < ApplicationRecord
   has_many :order_items
   belongs_to :category
   accepts_nested_attributes_for :document
-  # searchable do
-  #     text :title, :base_price
-  #     time :created_at
-  #     string :base_price
-  #     text :document do
-  #       document.photo_file_name unless document.blank?
-  #     end
-  # end
-  def self.search(str)
-  unless  str.blank?
-    if str.is_a?(String)
-      where("title like '%#{str}%'")
-    elsif str.is_a?(BigDecimal)
-       where(price: str.to_d)
-    end
-  else
-    all
+  searchable do
+      text :title, :base_price
+      time :created_at
+      string :base_price
+      text :document do
+        document.photo_file_name unless document.blank?
+      end
   end
-end
   def base_price
     price.to_i
   end

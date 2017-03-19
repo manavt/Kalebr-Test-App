04e6d8062e1fb630b72d5a5db9e3377af667dd94
manavt <manav.manav.smce@gmail.com>
2017-03-19 15:03:27 +0530

    jquery was not loading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,15 +17,15 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
-
+  #config.serve_static_files = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
-  config.assets.precompile
+  config.assets.precompile = true
   #{}` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.

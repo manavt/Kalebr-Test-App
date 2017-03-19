b6ebac0c3ee38c90662fe7f89b331f36047b459d
Manvendra Tripathi <manav.manav.vtu@gmail.com>
2017-03-19 14:11:25 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.js?}
   before_action :authenticate_user!
   rescue_from CanCan::AccessDenied do | exception |
    redirect_to root_url, alert: exception.message
